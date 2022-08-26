eeb5e9d73bf9d8deae83d51de440cddbe5f6c320
vladdubrovskyi <100083701+vladdubrovskyi@users.noreply.github.com>
2022-08-26 18:58:16 +0000

    Deploying to gh-pages from @ vladdubrovskyi/goit-js-hw-09@49bebe2ac2f13291e2e9ca9cac8a98352702bcd7 🚀

---
+++ 01-color-switcher.27192637.js
@@ -0,0 +1,2 @@
+const o=document.querySelector("[data-start]");console.log(o);const t=document.querySelector("[data-stop]");console.log(t);
+//# sourceMappingURL=01-color-switcher.27192637.js.map
---
+++ 01-color-switcher.dee7403a.js
@@ -0,0 +1,2 @@
+!function(){var o=document.querySelector("[data-start]");console.log(o);var e=document.querySelector("[data-stop]");console.log(e)}();
+//# sourceMappingURL=01-color-switcher.dee7403a.js.map
