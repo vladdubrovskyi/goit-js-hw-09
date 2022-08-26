f40e4d21ced7b32d79014cc33799d3f3f620f316
vladdubrovskyi <vladdubrovskyi1993@gmail.com>
2022-08-26 22:49:21 +0300

    finishes 1 task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,4 +1,29 @@
 const btnStart = document.querySelector('[data-start]');
-console.log(btnStart);
 const btnStop = document.querySelector('[data-stop]');
-console.log(btnStop);
+const body = document.querySelector('body');
+
+let intervalId = null;
+btnStop.setAttribute('disabled', 'true');
+
+btnStart.addEventListener('click', () => {
+  intervalId = setInterval(onBtnStart, 1000);
+});
+
+function onBtnStart() {
+  body.style.backgroundColor = getRandomHexColor();
+
+  btnStart.setAttribute('disabled', 'true');
+  btnStop.removeAttribute('disabled');
+}
+
+btnStop.addEventListener('click', onBtnStop);
+
+function onBtnStop() {
+  clearInterval(intervalId);
+  btnStart.removeAttribute('disabled');
+  btnStop.setAttribute('disabled', 'true');
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
